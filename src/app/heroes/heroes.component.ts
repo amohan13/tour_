348f7421fdc43518b448ed45b436b8ca1e2d5b76
amohan13 <anandmohanca31@gmail.com>
2018-03-14 17:41:00 +0530

    adding msg and service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-hero';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-heroes1=HEROES;
+heroes1:Hero[];
 selectedHero: Hero;
 
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
-  constructor() { }
-
-  ngOnInit() {}
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes1 => this.heroes1 = heroes1);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
   }
